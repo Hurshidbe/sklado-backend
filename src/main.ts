addffbc2e3715077e043bb5f9aeae70a2f1c7d8e
hurshidbe <egamberdiyevx8@gmail.com>
2025-11-18 13:10:50 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-dotenv.config()
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import cookieParser from "cookie-parser";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
-  app.useGlobalPipes(new ValidationPipe({
-    transform : true,
-    whitelist : true,
-    forbidNonWhitelisted : true
-  }))
+
+  app.enableCors({
+    origin: true,      
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
-  .setTitle('Toycha.uz APIs')
-  .setDescription('Toycha.uz loyihasining barcha endpointlari')
-  .setVersion('1.0')
-  .build()
-  const documentation = () => SwaggerModule.createDocument(app , config)
-  SwaggerModule.setup('apis' ,app , documentation, {jsonDocumentUrl : 'apis-json'})
+    .setTitle('Toycha.uz APIs')
+    .setDescription('Toycha.uz loyihasining barcha endpointlari')
+    .setVersion('1.0')
+    .build();
+
+  const documentation = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apis', app, documentation, { jsonDocumentUrl: 'apis-json' });
+
   await app.listen(process.env.PORT ?? 3000);
-  console.warn(`server is running port on ${process.env.PORT}`)
+  console.warn(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
