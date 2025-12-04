f78f7e14cd1ac881129ae4bd7c51ac0dfb2f81fe
hurshidbe <egamberdiyevx8@gmail.com>
2025-12-04 19:00:52 +0500

    mimor change

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,      
+    origin: process.env.ORIGIN|| console.log('origin is not defined'),      
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
